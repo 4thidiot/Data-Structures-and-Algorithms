cfbf74c8fb72994a8a483a90cffa7b13c0ea1830
4thidiot <tendingtowardsinfinity@gmail.com>
2025-09-18 16:34:34 +0530

    Create Reversing.cpp

---
+++ Linked List/Singly Linked List/Reversing.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int data;
+        Node *next;
+};
+
+class LinkedList
+{
+    private:
+        Node *first;
+    public:
+        Node *t, *last;
+        
+        LinkedList(int A[], int n)
+        {
+            first = new Node;
+            first->data = A[0];
+            first->next = NULL;
+            last = first;
+        
+            for(int i=1; i<n; i++)
+            {
+                t = new Node;
+                t->data = A[i];
+                t->next = NULL;
+                last->next = t;
+                last = t;
+            }
+        }
+        
+        void display()
+        {
+            Node *l;
+            l = first;
+            while(l!=NULL)
+            {
+                cout<<l->data<<" ";
+                l = l->next;
+            }
+            cout<<'\n';
+        }
+        
+        void Reverse()
+        {
+            Node *p, *q, *r;
+            p = first;
+            q = NULL;
+            r = NULL;
+            
+            while(p!=NULL)
+            {
+                r = q;
+                q = p;
+                p = p->next;
+                
+                q->next = r;
+            }
+            first = q;
+        }
+};
+
+int main() 
+{
+	int n;
+	cin>>n;
+	int v[n];
+	for(int i=0; i<n; i++)
+	    cin>>v[i];
+	    
+	LinkedList l1(v, n);
+	l1.display();
+	l1.Reverse();
+	l1.display();
+	
+    return 0;
+}
