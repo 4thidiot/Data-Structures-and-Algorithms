75fee3c0eb3e4557326eb537bf476122d1730cf3
4thidiot <tendingtowardsinfinity@gmail.com>
2025-09-18 19:52:07 +0530

    Create Displaying.cpp

---
+++ Linked List/Circular Linked List/Displaying.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+    public:
+        int data;
+        node *next;
+}*Head;
+
+void create(int A[], int n)
+{
+    node *t, *last;
+    
+    Head = new node;
+    Head->data = A[0];
+    Head->next = Head;
+    last = Head;
+    
+    for(int i=1; i<n; i++)
+    {
+        t = new node;
+        t->data = A[i];
+        t->next = Head;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display()
+{
+    node *p;
+    p = Head;
+    
+    do
+    {
+        cout<<p->data<<" ";
+        p = p->next;
+    }while(p!=Head);
+}
+
+int main() 
+{
+	int A[5] = {1, 2, 3, 4, 5};
+	create(A, 5);
+    Display();
+}
